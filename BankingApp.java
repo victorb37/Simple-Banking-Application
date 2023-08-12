c51578626fffe57b0ae801f98ad13eb3f718f63a
victorb37 <victorbarboza35@gmail.com>
2023-08-12 00:31:03 -0700

    Swapped Account1 and Account2

--- BankingApp.java
+++ BankingApp.java
@@ -3,27 +3,27 @@ public class BankingApp {
 
 	public static void main(String[] args) {
 		
-		AccountInfo account2 = new AccountInfo("87654321", 0.0, "Michael", "Jackson", "4321");
-		System.out.println(account2.getFirstName() + " " + account2.getLastName());
-		System.out.println("Account Number: " + account2.getAccountNum());
-		System.out.println("Pin Hash: " + account2.getPinHash());
-		account2.depositBalance(100000);
-		account2.withdrawBalance(13123.79);
-		account2.getBalance();
+		AccountInfo account1 = new AccountInfo("87654321", 0.0, "Michael", "Jackson", "4321");
+		System.out.println(account1.getFirstName() + " " + account1.getLastName());
+		System.out.println("Account Number: " + account1.getAccountNum());
+		System.out.println("Pin Hash: " + account1.getPinHash());
+		account1.depositBalance(100000);
+		account1.withdrawBalance(13123.79);
+		account1.getBalance();
 		
 		
 		System.out.println("\n\n");
 		
 		
-		AccountInfo account1 = new AccountInfo("12345678", 0.0, "Sam", "Smith", "1234");
-		account1.depositBalance(1000.00);
-		account1.getBalance();
+		AccountInfo account2 = new AccountInfo("12345678", 0.0, "Sam", "Smith", "1234");
+		account2.depositBalance(1000.00);
+		account2.getBalance();
 		System.out.println();
-		account1.withdrawBalance(5000.00);
+		account2.withdrawBalance(5000.00);
 		System.out.println();
-		account1.depositBalance(20000.00);
-		account1.withdrawBalance(5000.00);
-		account1.getBalance();
+		account2.depositBalance(20000.00);
+		account2.withdrawBalance(5000.00);
+		account2.getBalance();
 		
 	}
 
